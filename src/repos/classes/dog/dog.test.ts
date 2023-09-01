77df2ca36cf6db1b7826d27a494487a27260ce4d
Arthur Coelho <arthurcoelho7885@gmail.com>
2023-09-01 17:23:27 -0300

    feat: trying new sonar

--- src/repos/classes/dog/dog.test.ts
+++ src/repos/classes/dog/dog.test.ts
@@ -4,4 +4,8 @@ describe('dog', () => {
   it('dog bark', () => {
     expect(new Dog().bark()).toBe('auau');
   })
+
+  it('dog walk', () => {
+    expect(new Dog().walk()).toBe('walked');
+  })
 })
