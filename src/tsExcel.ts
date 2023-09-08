43e4a046b23e34be9127ff6ffb188210e06259cd
Arthur Coelho <arthurcoelho7885@gmail.com>
2023-09-08 16:53:50 -0300

    feat: ts check

--- src/tsExcel.ts
+++
@@ -1,7 +0,0 @@
-type AllowedTypes = string | string[] | boolean | number;
-
-type Xd = [string, boolean, number];
-
-function Parser<T extends AllowedTypes[]>(array: T[]) {}
-
-const xd = Parser<Xd>([['Jhel', true, 12], ['arthur', 12, '']])
