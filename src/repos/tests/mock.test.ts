391abea484a83422d6aa6d163eb78f4a1705395f
Arthur Coelho <arthurcoelho7885@gmail.com>
2023-10-11 12:23:25 -0300

    feat: added more tests to check coverage

--- src/repos/tests/mock.test.ts
+++ src/repos/tests/mock.test.ts
@@ -3,11 +3,16 @@ import { UserRepository } from "../originalClass";
 
 describe('xd', () => {
   it('xd', async () => {
-    const userRepo1 = mockResolvedClass(UserRepository, { getAvailable: true });
+    const userRepo1 = mockResolvedClass(
+      UserRepository, { getAvailable: true, getUsers: { name: "arthur", password: '123444'}},
+    );
 
 
     const response = userRepo1.getAvailable('xd');
 
+    const response2 = userRepo1.getUsers('xd');
+    
     expect(response).toEqual(true);
+    expect(response2).toStrictEqual({ name: "arthur", password: '123444'});
   })
 });
